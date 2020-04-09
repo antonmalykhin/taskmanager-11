e7659ce3ba33f6dab667e46eeb379aa62416a776
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-10 01:39:41 +0300

    Добавляет модуль с константами

---
+++ src/const.js
@@ -0,0 +1,43 @@
+const COLORS = [
+  `black`,
+  `blue`,
+  `green`,
+  `pink`,
+  `yellow`
+];
+
+const DAYS = [
+  `mo`,
+  `tu`,
+  `we`,
+  `th`,
+  `fr`,
+  `sa`,
+  `su`
+];
+
+const FILTER_NAMES = [
+  `all`,
+  `overdue`,
+  `today`,
+  `favorites`,
+  `repeating`,
+  `archive`
+];
+
+const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export {COLORS, DAYS, FILTER_NAMES, MONTH_NAMES};
