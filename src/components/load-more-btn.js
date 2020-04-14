bece40e5a7cfd2a0d0990b49cf81b8837bce9d06
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-14 22:00:22 +0300

    Добавляет и экспортирует класс

--- src/components/load-more-btn.js
+++ src/components/load-more-btn.js
@@ -1,5 +1,29 @@
+import {createElement} from '../utils.js';
+
 const createLoadButtonTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
 
-export {createLoadButtonTemplate};
+class LoadButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createLoadButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default LoadButton;
