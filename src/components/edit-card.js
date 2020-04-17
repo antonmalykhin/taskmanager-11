1d9ddfc952324b73df44a4e4cf4fabcf6ddacf40
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-17 20:20:52 +0300

    Наследуется от AbstractComponent, добаляте метод обработчик событий

--- src/components/edit-card.js
+++ src/components/edit-card.js
@@ -1,5 +1,6 @@
-import {COLORS, DAYS, MONTH_NAMES} from "../const.js";
-import {createElement, formatTime} from "../utils.js";
+import {COLORS, DAYS, MONTH_NAMES} from '../const.js';
+import {formatTime} from '../utils/common.js';
+import AbstractComponent from './abstract-component.js';
 
 const createColorsMarkup = (colors, currentColor) => {
   return colors
@@ -131,26 +132,19 @@ const createEditCardTemplate = (card) => {
   );
 };
 
-class CardEdit {
+class CardEdit extends AbstractComponent {
   constructor(card) {
+    super();
+
     this._card = card;
-    this._element = null;
   }
 
   getTemplate() {
     return createEditCardTemplate(this._card);
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+  setSubmitHandler(handler) {
+    this.getElement().querySelector(`form`).addEventListener(`submit`, handler);
   }
 }
 
