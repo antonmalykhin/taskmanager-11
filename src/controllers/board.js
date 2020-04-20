e01c039de273f2e94269e673e1ecf2fc5d5ddb16
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-20 18:18:13 +0300

    Реализует сортировку
    Добавялет функцию renderCards
    Добавляет функцию getSortedCards
    Добавялте в конструктор инстансы компанентов
    Добавялте в BoardController метод render из функции renderBoard

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -2,7 +2,7 @@ import CardsComponent from '../components/cards.js';
 import CardComponent from '../components/card.js';
 import CardEditComponent from '../components/edit-card.js';
 import NoCardsComponent from '../components/no-cards.js';
-import SortingComponent from '../components/sorting.js';
+import SortingComponent, {SortingType} from '../components/sorting.js';
 import LoadButtonComponent from '../components/load-more-btn.js';
 
 import {render, replace, remove, RenderPosition} from '../utils/render.js';
@@ -43,47 +43,91 @@ const renderCard = (cardListElement, card) => {
   render(cardListElement, cardComponent, RenderPosition.BEFOREEND);
 };
 
-const renderBoard = (boardComponent, cards) => {
-  const isAllCardsArchived = cards.every((card) => card.isArchive);
+const renderCards = (cardListElement, cards) => {
+  cards.forEach((task) => {
+    renderCard(cardListElement, task);
+  });
+};
 
-  if (isAllCardsArchived) {
-    render(boardComponent.getElement(), new NoCardsComponent(), RenderPosition.BEFOREEND);
-    return;
+const getSortedCards = (cards, sortingType, from, to) => {
+  let sortedCards = [];
+  const showingCards = cards.slice();
+
+  switch (sortingType) {
+    case SortingType.DATE_UP:
+      sortedCards = showingCards.sort((a, b) => a.dueDate - b.dueDate);
+      break;
+    case SortingType.DATE_DOWN:
+      sortedCards = showingCards.sort((a, b) => b.dueDate - a.dueDate);
+      break;
+    case SortingType.DEFAULT:
+      sortedCards = showingCards;
+      break;
   }
 
-  render(boardComponent.getElement(), new SortingComponent(), RenderPosition.BEFOREEND);
-  render(boardComponent.getElement(), new CardsComponent(), RenderPosition.BEFOREEND);
-
-  const cardListElement = boardComponent.getElement().querySelector(`.board__tasks`);
-
-  let showingCardsCount = SHOWING_CARDS_COUNT_ON_START;
-
-  cards.slice(0, showingCardsCount).forEach((card) => {
-    renderCard(cardListElement, card);
-  });
-
-  const loadMoreButtonComponent = new LoadButtonComponent();
-  render(boardComponent.getElement(), loadMoreButtonComponent, RenderPosition.BEFOREEND);
-
-  loadMoreButtonComponent.setClickHandler(() => {
-    const prevCardCount = showingCardsCount;
-    showingCardsCount = showingCardsCount + SHOWING_CARDS_COUNT_BY_BUTTON;
-
-    cards.slice(prevCardCount, showingCardsCount).forEach((card) => render(cardListElement, renderCard(cardListElement, card)));
-
-    if (showingCardsCount >= cards.length) {
-      remove(loadMoreButtonComponent);
-    }
-  });
+  return sortedCards.slice(from, to);
 };
 
 class BoardController {
   constructor(container) {
     this._container = container;
+
+    this._noCardsComponent = new NoCardsComponent();
+    this._sortingComponent = new SortingComponent();
+    this._cardsComponent = new CardsComponent();
+    this._loadMoreButtonComponent = new LoadButtonComponent();
   }
 
   render(cards) {
-    renderBoard(this._container, cards);
+    const renderLoadMoreButton = () => {
+      if (showingCardsCount >= cards.length) {
+        return;
+      }
+
+      render(container, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
+
+      this._loadMoreButtonComponent.setClickHandler(() => {
+        const prevCardsCount = showingCardsCount;
+        showingCardsCount = showingCardsCount + SHOWING_CARDS_COUNT_BY_BUTTON;
+
+        const sortedCards = getSortedCards(cards, this._sortingComponent.getSortingType(), prevCardsCount, showingCardsCount);
+
+        renderCards(cardListElement, sortedCards);
+
+        if (showingCardsCount >= cards.length) {
+          remove(this._loadMoreButtonComponent);
+        }
+      });
+    };
+
+    const isAllCardsArchived = cards.every((card) => card.isArchive);
+    const container = this._container.getElement();
+
+    if (isAllCardsArchived) {
+      render(container, this._noCardsComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    render(container, this._sortingComponent, RenderPosition.BEFOREEND);
+    render(container, this._cardsComponent, RenderPosition.BEFOREEND);
+
+    const cardListElement = this._cardsComponent.getElement();
+
+    let showingCardsCount = SHOWING_CARDS_COUNT_ON_START;
+
+    renderCards(cardListElement, cards.slice(0, showingCardsCount));
+    renderLoadMoreButton();
+
+    this._sortingComponent.setSortingTypeChangeHandler((sortingType) => {
+      showingCardsCount = SHOWING_CARDS_COUNT_BY_BUTTON;
+
+      const sortedCards = getSortedCards(cards, sortingType, 0, showingCardsCount);
+
+      cardListElement.innerHTML = ``;
+
+      renderCards(cardListElement, sortedCards);
+      renderLoadMoreButton();
+    });
   }
 }
 
