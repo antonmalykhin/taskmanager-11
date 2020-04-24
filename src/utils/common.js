1b9318ea8032e3c059f3c6a3e60a668150975a20
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-24 23:31:15 +0300

    Добавляет отображение даты и время с помощью moment

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const getRandomArrayItem = (array) => {
   const randomIndex = getRandomIntegerNumber(0, array.length);
 
@@ -18,15 +20,12 @@ const getRandomDate = () => {
   return targetDate;
 };
 
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
-};
-
 const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours() % 12);
-  const minutes = castTimeFormat(date.getMinutes());
-
-  return `${hours}:${minutes}`;
+  return moment(date).format(`hh:mm`);
 };
 
-export {formatTime, getRandomArrayItem, getRandomDate};
+const formatDate = (date) => {
+  return moment(date).format(`DD MMMM`);
+};
+
+export {formatTime, formatDate, getRandomArrayItem, getRandomDate};
